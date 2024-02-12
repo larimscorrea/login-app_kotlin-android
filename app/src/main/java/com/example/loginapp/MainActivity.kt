572a057fec4e6e5cc365de07c648329615bde681
Larissa Corrêa <larimscorrea@gmail.com>
2024-02-12 00:50:46 -0300

    tem erro

--- app/src/main/java/com/example/loginapp/MainActivity.kt
+++ app/src/main/java/com/example/loginapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.loginapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnCLickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i( tag: "Teest Credentials", msg: "Username" : $username and Password : $password)
+
+        }
     }
 }
